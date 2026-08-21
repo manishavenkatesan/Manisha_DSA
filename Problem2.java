3ec983c3b2599a9c72be610ab90416cdf54ed05d
manishavenkatesan <manishavenkatesan20@gmail.com>
2026-08-21 18:27:16 +0530

    Create Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int s1=sc.nextInt();
+      int s2=sc.nextInt();
+      int s3=sc.nextInt();
+      if(s1+s2+s2 == 180){
+        System.out.println("Triangle can be formed");
+
+      }
+      else{
+        System.out.println("Triangle cannot be formed");
+      }
+      
+    }
+}
