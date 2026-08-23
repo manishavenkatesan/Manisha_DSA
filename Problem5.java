6fcc3fecae09463ecfdb6ed2e42703cba0c20fe5
manishavenkatesan <manishavenkatesan20@gmail.com>
2026-08-23 09:22:54 +0530

    Create Problem5.java

---
+++ Problem5.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc =new Scanner(System.in);
+      int x=sc.nextInt();
+      int y=sc.nextInt();
+      for(int i=0;i<y;i++){
+        System.out.println(x);
+      }
+    }
+}
+//Write a program which takes two values x and y. Prints x for y number of times.
